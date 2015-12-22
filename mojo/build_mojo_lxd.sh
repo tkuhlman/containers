e91b0ed0f6a1980fbd6ec14b09776341eef28ddc
Timothy Kuhlman <timothy.kuhlman@canonical.com>
2015-12-21 17:12:03 -0700

    The LXD team helped me overcome the nesting problem! Still a bit of work for unprivileged.

--- mojo/build_mojo_lxd.sh
+++ mojo/build_mojo_lxd.sh
@@ -5,26 +5,33 @@
 # Pre-reqs:
 #   - An image named trusty - https://linuxcontainers.org/lxd/getting-started-cli/
 
-#Launch
-lxc launch trusty mojo -c security.privileged=true -c security.nesting=true
+#Launch the container
+#name=mojo-$(date +%Y-%m-%d)  # Useful if you are testing multiple versions.
+name=mojo
+
+lxc launch trusty $name -c security.nesting=true
+# privileged works but is obviously isn't preferred
+#lxc launch trusty mojo -c security.privileged=true -c security.nesting=true
 sleep 5  # Takes a moment for the network
 
 # Pre-req install
-lxc exec mojo -- apt-get update
-lxc exec mojo -- apt-get install -y software-properties-common
-lxc exec mojo -- apt-add-repository -y ppa:mojo-maintainers/ppa
-lxc exec mojo -- apt-get update
-lxc exec mojo -- apt-get dist-upgrade -y
-lxc exec mojo -- apt-get install -y mojo bzr juju-local
+lxc exec $name -- apt-get update
+lxc exec $name -- apt-get install -y software-properties-common
+lxc exec $name -- apt-add-repository -y ppa:mojo-maintainers/ppa
+# The latest or at least backports version of lxc is needed for working with nesting.
+lxc exec $name -- apt-add-repository -y ppa:ubuntu-lxc/daily
+lxc exec $name -- apt-get update
+lxc exec $name -- apt-get dist-upgrade -y
+lxc exec $name -- apt-get install -y mojo bzr juju-local devscripts debhelper python-all
 
 #juju bootstrap
-lxc file push ubuntu.sudoers mojo/etc/sudoers.d/ubuntu
-lxc exec mojo -- su ubuntu -c 'juju generate-config'
-lxc exec mojo -- su ubuntu -c 'juju switch local'
-lxc exec mojo -- su ubuntu -c 'juju bootstrap'
+lxc file push ubuntu.sudoers $name/etc/sudoers.d/ubuntu
+lxc exec $name -- su ubuntu -c 'juju generate-config'
+lxc exec $name -- su ubuntu -c 'juju switch local'
+lxc exec $name -- su ubuntu -c 'juju bootstrap'
 
 # Setup test scripts
-lxc file push mojo-integration-test.sh mojo/home/ubuntu/mojo-itegration-test.sh
+lxc file push mojo-integration-test.sh $name/home/ubuntu/mojo-itegration-test.sh
 
-echo "Now run a shell with 'lxc exec mojo -- su - ubuntu'"
+echo "Now run a shell with 'lxc exec $name -- su - ubuntu'"
 echo "Then you can branch mojo install it and run integration tests, or branch a new spec to test."
--- mojo/mojo-integration-test.sh
+++ mojo/mojo-integration-test.sh
@@ -2,5 +2,19 @@
 #
 # Run a set of integration tests with the currently installed mojo
 
+mkdir $1
+cd $1
+bzr branch $1 mojo
+cd mojo/debian
+debuild --no-tgz-check -sa -I.bzr -us -uc
+sudo dpkg -i ../../mojo*.deb
+
+## Mojo how to spec test
+# Note the manual lxc-create is needed because of the nesting in an unprivileged container
+#   https://www.stgraber.org/2014/01/17/lxc-1-0-unprivileged-containers/
+sudo lxc-create -t download -n mojo-how-to.trusty -B dir --dir /srv/mojo/mojo-how-to-trusty/ROOTFS -- -d ubuntu -r trusty -a amd64
 sudo mojo project-new --series trusty mojo-how-to
+sudo chmod 755 /var/lib/lxc/mojo-how-to.trusty && sudo chmod 755 /var/lib/lxc
 mojo workspace-new --project mojo-how-to --stage=mojo-how-to/devel --series trusty lp:~mojo-maintainers/mojo/mojo-specs mojo-how-to
+sudo apt-get install python-swiftclient
+mojo run --project mojo-how-to --series trusty --stage mojo-how-to/devel lp:~mojo-maintainers/mojo/mojo-specs mojo-how-to
